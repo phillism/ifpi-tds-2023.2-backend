84f6a7bd5a2e8d5d13263f67e8ba4d55a85f786d
phillism <phill.leamsi@gmail.com>
2023-09-21 09:37:58 -0300

    Requirements atualizado

--- fastapi/smarthome-api/persistence/utils.py
+++ fastapi/smarthome-api/persistence/utils.py
@@ -1,10 +1,8 @@
 from sqlmodel import create_engine
-
+from decouple import config
 
 def obter_engine():
-  db_url = 'sqlite:///database.db'
-  # db_url = 'postgresql+psycopg2://smarthome_b2hz_user:pmtszsfMU1fKevP6GYrznZdvJHxX2007@dpg-cjsd4rktjf3s73biv9tg-a.oregon-postgres.render.com/smarthome_b2hz'
-  #db_url = 'postgresql+psycopg2://postgres:postgres@localhost:5432/smarthome'
+  db_url = config("DATABASE_URI")
   engine = create_engine(db_url, echo=True)
   
   return engine
