dd8a2382ae53f75df1af4d02181429c59b9dfef5
phillism <phill.leamsi@gmail.com>
2023-09-21 09:23:56 -0300

    Biblioteca de .env alterada

--- fastapi/rinha-api/persistence/utils.py
+++ fastapi/rinha-api/persistence/utils.py
@@ -1,11 +1,8 @@
-from dotenv import dotenv_values, load_dotenv
+from decouple import config
 from sqlmodel import create_engine
 
-load_dotenv()
-config = dotenv_values(".env")
-
 def get_engine():
-    db_url = 'sqlite:///database.db' if "DATABASE_URI" not in config else config["DATABASE_URI"]
+    db_url = config("DATABASE_URI")
     engine = create_engine(db_url, echo=True)
 
     return engine
