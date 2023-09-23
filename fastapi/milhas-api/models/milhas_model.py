cb80a1f5e86fdbc0ba2e44e4eee7816ab533a631
phillism <phill.leamsi@gmail.com>
2023-09-23 11:13:49 -0300

    Arrumado internal error 'division_by_zero'

--- fastapi/milhas-api/models/milhas_model.py
+++ fastapi/milhas-api/models/milhas_model.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 class SimulacaoMilha(BaseModel):
-    quantidade: int = Field(multiple_of=1000)
+    quantidade: int = Field(multiple_of=1000, gt=0)
     desconto: float = Field(ge=0, le=80)
     bonus: float = Field(ge=0, le=300)
 
